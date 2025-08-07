8d2514bbed63db5479f890fb1d4eef8cecd74cb6
GaryGould <garydongould@gmail.com>
2025-08-07 15:13:18 -0700

    DISABLED AUTOCAPTURE

--- utils/posthog.ts
+++ utils/posthog.ts
@@ -7,7 +7,7 @@ let posthog: any = null;
 if (Platform.OS === 'web' && typeof window !== 'undefined') {
   posthog = posthogJS.init('phc_KvxX7mU1WMHNh1lQsLUyryTaRBzTS3nUiP2b0tqQU6U', {
     api_host: 'https://us.i.posthog.com',
-    autocapture: true, // Enable to capture clicks and form submissions
+    autocapture: false, // Disable automatic event capture
     capture_pageview: true, // Enable to start sessions automatically
     capture_pageleave: true, // Enable to track when users leave
     session_recording: {
